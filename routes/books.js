39e41a0245005bdbc7843b453f45b4a9e37ab45c
RoxanneLottier <roxanneleger@msn.com>
2023-08-09 14:09:52 +0200

    routes made & auth made but nothing is working yet

---
+++ routes/books.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const bookCtrl = require('../controllers/books');
+
+router.post('/', bookCtrl.createBook);
+router.put('/:id', bookCtrl.modifyBook);
+router.delete('/:id', bookCtrl.deleteBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.get('/', bookCtrl.getAllBooks);
+
+module.exports = router;
